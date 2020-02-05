741250ea0d198ddfbd0a8da7fecd764928919093
Extrieve <nicolas.cinera@gmail.com>
2020-02-04 23:58:24 -0500

    Player Movement Script
    Added basic player character movement to a test scene. Feel free to move stuff around

---
+++ Dijkstra-Pilots/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector2 movement; 
+
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+
+    
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
