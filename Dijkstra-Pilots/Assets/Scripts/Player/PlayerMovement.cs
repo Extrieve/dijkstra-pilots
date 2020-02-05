2b804298cfed8df5562061122ae0ef3ad03fcac5
Nick <nicolas.cinera@gmail.com>
2020-02-05 14:08:50 -0500

    Update PlayerMovement scrip
    Now the player character moves pointing towards the cursor at all times. This is done in preparation for the next step of shooting projectiles from the player.

--- Dijkstra-Pilots/Assets/Scripts/Player/PlayerMovement.cs
+++ Dijkstra-Pilots/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Vector2 movement; 
+    private Vector2 movement, mousePos; 
 
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
+    public Camera camera;
 
-    
     void Update()
     {
+        //Basic 2D movement
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        //Feeding mouse position data to mousePos variable
+        mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        //Finding the intermediate vector, so that the character model aims 
+        // where the cursor is located
+        Vector2 lookDirection = mousePos - rb.position; 
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
