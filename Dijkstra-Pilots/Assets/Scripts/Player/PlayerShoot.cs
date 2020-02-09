22a1a62fef841187d04baccb3c389f199562e1ea
Nick <nicolas.cinera@gmail.com>
2020-02-09 10:37:59 -0500

    Testing Player Shooting
    I was bored...

--- Dijkstra-Pilots/Assets/Scripts/Player/PlayerShoot.cs
+++ Dijkstra-Pilots/Assets/Scripts/Player/PlayerShoot.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform firePosition;
+    public GameObject bulletPrefab;
+
+    public float bulletForce = 20f; 
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePosition.position, firePosition.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePosition.up * bulletForce, ForceMode2D.Impulse);
     }
 }
