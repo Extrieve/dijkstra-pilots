671586787c77734515b2e6d86d1edf72c9f2dbd6
nikolayiotchev <60583103+nikolayiotchev@users.noreply.github.com>
2020-03-29 13:54:17 -0400

    Added more Audio
    Only settings button and enemy shooting left. For now...

--- Dijkstra-Pilots/Assets/Scripts/Level/StartGame.cs
+++ Dijkstra-Pilots/Assets/Scripts/Level/StartGame.cs
@@ -5,13 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
+    public AudioSource buttonSound;
+
     public void gameBegin()
     {
+        buttonSound.Play();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void exitGame()
     {
+        buttonSound.Play();
         Application.Quit();
         Debug.Log("Quit Game");
     }
---
+++ Dijkstra-Pilots/Assets/Scripts/Menu Button Sound/buttonFx.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonFx : MonoBehaviour
+{
+
+    public AudioSource fX;
+    public AudioClip buttonSound;
+   
+    public void ButtonSound()
+    {
+        fX.PlayOneShot(buttonSound);
+    }
+}
