23aa9c1bf566bfe38f2228b2eb041387f0ccc190
Nick <nicolas.cinera@gmail.com>
2020-04-11 13:44:16 -0400

    Added Pause Menu Functionality
    It's not looking 'good' but it is working, you can activate the pause by pressing escape.

---
+++ Dijkstra-Pilots/Assets/Scripts/Managers/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuInterface;
+    [SerializeField] private bool isPaused;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+
+        if (isPaused)
+        {
+            ActivatePause();
+        }
+
+        else
+        {
+            ResumeGame();
+        }
+    }
+
+    private void ActivatePause()
+    {
+        Time.timeScale = 0;
+        AudioListener.pause = true;
+        pauseMenuInterface.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        pauseMenuInterface.SetActive(false);
+        isPaused = false;
+    }
+}
